55d50272ed0e9503b4c7de7648440d177aa54106
Olivier ROLAND <olivier.roland@edla.org>
2012-11-16 00:32:08 +0100

    Dirty implementation for FIFO with constant size

--- src/main/scala/org/edla/ambassy/service/cache/CacheServiceActor.scala
+++ src/main/scala/org/edla/ambassy/service/cache/CacheServiceActor.scala
@@ -5,16 +5,32 @@ import akka.actor.ActorLogging
 import com.redis._
 
 object CacheService {
-  
+  //22711
   val r = new RedisClient("localhost", 6379)
-  
+  val maxCache = 10
+  var currentCache = 0
+
   case class Push(elem: String)
 
   class CacheServiceActor extends Actor with ActorLogging {
     def receive = {
       case Push(elem) ⇒ {
-        log.info("Push " + elem)
-        r.lpush("fifo", elem)
+        log.info(currentCache + " Push " + elem)
+        val in = elem.split(":")
+        if (in(0).toInt <= maxCache) {
+          currentCache = currentCache + in(0).toInt
+          var toFree = currentCache - maxCache
+          while (toFree > 0) {
+            val out = r.rpop("fifo") match {
+              case Some(foo) => foo.split(":")
+              case None => "0: ".split(":")
+            }
+            log.info(currentCache + " Pop " + out(1))
+            currentCache = currentCache - out(0).toInt
+            toFree = toFree - out(0).toInt
+          }
+          r.lpush("fifo", elem)
+        }
       }
     }
   }
